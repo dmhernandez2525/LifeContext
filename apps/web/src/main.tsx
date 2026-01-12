2e222a804de675f7f6a3db8a41f918b74ac26d96
Daniel Hernandez <danielmark252525@GMAIL.COM>
2026-01-12 15:22:34 -0500

    fix(web): navigation scroll reset and sticky header overlapping

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ScrollToTop from './components/ScrollToTop';
 import { ToastProvider } from './components/Toast';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <ToastProvider>
         <App />
       </ToastProvider>
